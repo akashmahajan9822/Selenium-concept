3414694d483ebfaa5a6682a1657e6e31fe435e24
58825_Ashish <mahajanashish1999@gmail.com>
2022-01-24 20:54:44 +0530

    update log4j

--- src/log4jConcept/login.java
+++ src/log4jConcept/login.java
@@ -17,17 +17,18 @@ import org.testng.annotations.Test;
 
 public class login {
 	 ChromeDriver driver;
-	
+	 Logger log ;
 	 
 	@BeforeClass
 	public void setup() {
-		 Logger log = Logger.getLogger(login.class);
+		  log = Logger.getLogger(login.class);
 		  PropertyConfigurator.configure("log4j.properties");  // configure log4j.properties file
 		 // DOMConfigurator.configure("log4j.xml");       //// configure log4j.xml file
-		  
+		  log.info("****************************** Starting test cases execution  *****************************************");
+			  
 		  
 		 System.setProperty("webdriver.chrome.driver", "C:\\Selenium jar\\chromeDriverfor96.0\\chromedriver.exe");
-			
+		 log.info("launching chrome broswer");
 		     driver =new ChromeDriver();    ////Step-1 Browser open- by creating
 		
 		    driver.manage().window().maximize();
@@ -37,24 +38,36 @@ public class login {
 			driver.manage().deleteAllCookies();
 		    driver.get("https://classic.freecrm.com/index.html"); 
 		
+		    
+		    
+		    driver.get("https://classic.freecrm.com/index.html");
+			log.info("entering application URL");
+			log.warn("Hey this just a warning message");
+			log.fatal("hey this is just fatal error message");
+			log.debug("this is debug message");
 	}
 @Test
 public void validatTitle() {
+	log.info("****************************** starting test case *****************************************");
+	log.info("****************************** freeCrmTitleTest *****************************************");
 	String tt = driver.getTitle();
+	log.info("login page title is--->"+ tt);
 	Assert.assertEquals(driver.getTitle(), tt,"home page title fail");
-	
+	log.info("****************************** freeCrmTitleTest closed*****************************************");
 }
 	
 @Test
 public void validatURL() {
-	
+	log.info("****************************** freeCrmURLTest *****************************************");
 	
 	Assert.assertEquals(driver.getCurrentUrl(), "https://classic.freecrm.com/index.html","home page URl fail");
-	
+	log.info("****************************** freeCrmURLTestclosed *****************************************");
+	log.info("****************************** ending test case *****************************************");
 }
 	@AfterClass
 	public void teardown() {
 		
 		driver.quit();
+		log.info("****************************** Browser is closed *****************************************");
 	}
 }
